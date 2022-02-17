eb52007b1407832f19c35ac54159057c752595de
vaibhavi upadhyay <70330367+vai12376@users.noreply.github.com>
2022-02-17 12:25:20 +0530

    added internal template and style

--- angular.practise/angular-practise-test/src/app/module/test/success/success.component.ts
+++ angular.practise/angular-practise-test/src/app/module/test/success/success.component.ts
@@ -2,8 +2,17 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-success',
-  templateUrl: './success.component.html',
-  styleUrls: ['./success.component.scss']
+  template:`<div class="success">
+    <p>
+        in success component
+    </p>
+</div>`,
+  styles: [`.success {
+    background-color: lightgreen;
+    color: darkgreen;
+    height: 200px;
+    width: 400px;
+}`]
 })
 export class SuccessComponent{
 
