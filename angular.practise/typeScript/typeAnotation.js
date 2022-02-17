1cac6b9660cea05652885cd7a428d107fa64bceb
vaibhavi upadhyay <70330367+vai12376@users.noreply.github.com>
2022-02-17 18:34:18 +0530

    type annotation in typeScript

---
+++ angular.practise/typeScript/typeAnotation.js
@@ -0,0 +1,10 @@
+/** @format */
+var a = "";
+var getData = function (para1, para2) {
+    var b = para1 + para2;
+    return b;
+};
+getData("a", "b");
+var c;
+c = "str";
+c = 2;
---
+++ angular.practise/typeScript/typeAnotation.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+let a: string = "";
+
+const getData = function (para1: string, para2: string) {
+  let b = para1 + para2;
+  return b;
+};
+let c =getData("a", "b");
+console.log(c);
+
