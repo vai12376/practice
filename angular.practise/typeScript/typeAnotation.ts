b38f540fce46e8afe6f01d42f44a593bed7dc0c6
vaibhavi upadhyay <70330367+vai12376@users.noreply.github.com>
2022-02-17 18:40:49 +0530

    Update typeAnotation.ts

--- angular.practise/typeScript/typeAnotation.ts
+++ angular.practise/typeScript/typeAnotation.ts
@@ -1,11 +1,31 @@
-/** @format */
-
+//explicit
 let a: string = "";
+//typeScript will autometically inffer the type
+let b = "str";
 
+//if variable is not initialized yet then it's type will be any
+let d;
+
+//type anotation can be used into function parameter and retun value too
 const getData = function (para1: string, para2: string) {
   let b = para1 + para2;
   return b;
+  //implicitely return b as string
 };
-let c =getData("a", "b");
+let c = getData("a", "b");
 console.log(c);
+
+//type anotation in array
+
+let arr = [];
+
+let arr2: number[];
+// will not allow arr2=['a']
+
+//union type
+
+let u: string | object;
+//not allow u=2;
+u = "str";
+u = { k: 1 };
 
